a8f5ad6101ad01995b39b1c15e95b207daa3995d
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-09-12 14:11:01 -0500

    Add style to home page :haircut:

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ Cuba.plugin Cuba::Mote
 
 Dir['./routes/**/*.rb'].each { |f| require f }
 
+Cuba.use Rack::Static,
+  urls: %w(/css /img),
+  root: './public'
+
 Cuba.define do
   on default do
     run Statics
