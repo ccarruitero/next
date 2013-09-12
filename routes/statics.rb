bafc0ae083a05d83dc00ff8f00dd862e701667a7
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-09-12 13:51:35 -0500

    Add home page

--- routes/statics.rb
+++ routes/statics.rb
@@ -1,7 +1,9 @@
 class Statics < Cuba
+  settings[:mote][:views] = File.expand_path('views/statics', Dir.pwd)
+
   define do
     on root do
-      res.write "Hello World"
+      res.write view('home')
     end
   end
 end
