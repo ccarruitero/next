40952e0c36cd825d2082f2087c3e105e4522a7f1
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-09-13 12:57:11 -0500

    Deep roots are not reached by the frost.

--- app.rb
+++ app.rb
@@ -3,14 +3,12 @@ require 'cuba/contrib'
 
 Cuba.plugin Cuba::Mote
 
-Dir['./routes/**/*.rb'].each { |f| require f }
-
 Cuba.use Rack::Static,
   urls: %w(/css /img),
   root: './public'
 
 Cuba.define do
-  on default do
-    run Statics
+  on root do
+    res.write partial('statics/home')
   end
 end
--- routes/statics.rb
+++
@@ -1,9 +0,0 @@
-class Statics < Cuba
-  settings[:mote][:views] = File.expand_path('views/statics', Dir.pwd)
-
-  define do
-    on root do
-      render('home')
-    end
-  end
-end
