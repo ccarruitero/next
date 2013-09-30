36c187062646c22ddaac18f2041be204835f0218
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-09-30 01:01:35 -0500

    Set GitHub OAuth authorize url from ENV variable

--- lib/github.rb
+++ lib/github.rb
@@ -1,5 +1,5 @@
 module GitHub
   def self.authorize_url
-    "https://github.com/login/oauth/authorize?client_id=#{ GITHUB_CLIENT_ID }"
+    "#{ GITHUB_OAUTH_AUTHORIZE }?client_id=#{ GITHUB_CLIENT_ID }"
   end
 end
